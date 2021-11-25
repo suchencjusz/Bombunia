7bcad094effd6533bf930fe1c4f6eb2dcd1b49c3
suchencjusz <sucheta348@gmail.com>
2021-11-25 16:19:23 +0100

    minor
    tera sie zapisze nawet przedmiot bez ocen takie jaja :DDD

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ with open('config.json') as f:
     config = json.load(f)
     f.close()
 
+bombunia_ver="1.0.0-dev"
 color=""
 debuginfo=""
 grades_path = "grades/"
@@ -154,7 +155,8 @@ try:
     dzejson = r.json()
     print("dlugosc json", len(str(dzejson)))
     if len(str(dzejson)) < 500:
-        print("chuj")
+        print("brak uprawnien...")
+        ciasteczko.flush()
         ciasteczko.catch()
         r = requests.post(url, data=json.dumps(payload),
                           headers=headers) 
@@ -170,7 +172,10 @@ print(dzejson['data'])
 for subject in dzejson['data']:
     print(subject['Subject'], "- Brak ocen\n\n") if subject['TableContent'] == None else print(
         subject['Subject'], '-', HowMuchGrades(subject))
-    if subject['TableContent'] != None:
+    if subject['TableContent'] == None:
+        AllGrades.append(
+            {'subject_name': subject['Subject'], 'grades': [0,0,0,0,0,0]})
+    else:
         AllGrades.append(
             {'subject_name': subject['Subject'], 'grades': GradesToList(subject)})
 
@@ -226,16 +231,16 @@ for toCmpr in AllGradesFinal[0]['allGrades']:
     if toCmpr not in AllGradesToCompare[0]['allGrades']:
         comparingb.append(toCmpr)
 
-if len(comparinga) != len(comparingb):
-    print("comprainga != compraingb wtf?")
-    quit()
+# if len(comparinga) != len(comparingb):
+#     print("comprainga != compraingb wtf? ")
+#     quit()
 
 newgrades = []
 
 for i in range(len(comparinga)):
     tempciak = [None]*6
     for c in range(6):
-        tempciak[c] = int(comparinga[i]['grades'][c]) - int(comparingb[i]['grades'][c])
+        tempciak[c] = int(comparingb[i]['grades'][c]) - int(comparinga[i]['grades'][c])
     newgrades.append({'subject_name': comparinga[i]['subject_name'],
                       'grades': tempciak}
                      )
@@ -258,18 +263,18 @@ color = MuchPalasColor(sumOfPalas)
 
 print(toDiscordWebhook)
 
-#time_checked = datetime.fromtimestamp(AllGradesToCompare[0]['time'])
-
 average=""
 
 averageOld=round(AllGradesToCompare[0]['sumOfAllGrades']/AllGradesToCompare[0]['muchOfAllGrades'],5)
 averageNew=round(AllGradesFinal[0]['sumOfAllGrades']/AllGradesFinal[0]['muchOfAllGrades'],5)
 
 if averageOld<averageNew:
-    average = "Nasza średnia wzrosła z "+str(averageOld)+" do "+str(averageNew)
+    average = "Nasza średnia wzrosła z "
 else:
-    average = "Nasza średnia spadła z "+str(averageOld)+" do "+str(averageNew)
-    
+    average = "Nasza średnia spadła z "
+
+average += str(averageOld)+" do "+str(averageNew)+" (średnia nie uwzględnia wag)"
+
 print(average)
 
-SendToDiscord()
+if config["discord_webhook_f"] == True: SendToDiscord()
